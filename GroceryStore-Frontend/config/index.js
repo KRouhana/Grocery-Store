986bb1a3ba0c6fff82a4669b4840202fd207e843
Karl Rouhana <karl@rouhana.me>
2025-01-31 13:48:11 +0200

    Update to a local DB

--- GroceryStore-Frontend/config/index.js
+++ GroceryStore-Frontend/config/index.js
@@ -27,19 +27,14 @@ module.exports = {
   },
   dev: {
     env: require('./dev.env'),
-    host: '127.0.0.1',
+    host: 'localhost',
     port: 8087,
-    backendHost: '127.0.0.1',
+    backendHost: 'localhost',
     backendPort: 8080,
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     proxyTable: {},
-    // CSS Sourcemaps off by default because relative paths are "buggy"
-    // with this option, according to the CSS-Loader README
-    // (https://github.com/webpack/css-loader#sourcemaps)
-    // In our experience, they generally work as expected,
-    // just be aware of this issue when enabling this option.
     cssSourceMap: false
   }
 }
